725bb02203e0c11625b92234084fdad924546aad
Hazim ASRI <wan-muhammad-hazim.wan-mohd-asri@etu.univ-lyon1.fr>
2023-03-30 16:12:46 +0200

    Display comments and add comment to post

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -13,9 +13,9 @@ class CommentType extends AbstractType
     {
         $builder
             ->add('content')
-            ->add('valid')
-            ->add('createdAt')
-            ->add('post')
+            // ->add('valid')
+            // ->add('createdAt')
+            // ->add('post')
         ;
     }
 
