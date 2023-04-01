9c677418696e243911bfd0365ed9471752ab82f1
Hazim ASRI <wan-muhammad-hazim.wan-mohd-asri@etu.univ-lyon1.fr>
2023-04-01 14:35:02 +0200

    Add categories dynamically to header

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,15 +8,17 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PostRepository;
 use App\Entity\Post;
 use App\Form\PostType;
+use App\Repository\CategoryRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(PostRepository $postRepository): Response
+    public function index(PostRepository $postRepository, CategoryRepository $categoryRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'posts' => $postRepository->findAll(),
+            'categories' => $categoryRepository->findAll()
         ]);
     }
 }
